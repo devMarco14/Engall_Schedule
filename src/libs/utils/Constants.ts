ec4ee4c6c980f16417f82efef3baba7b3c807fb5
Marco <jonghyeok.lim14@gmail.com>
2022-07-28 23:48:13 +0900

    #7 design : add 페이지 퍼블리싱

---
+++ src/libs/utils/Constants.ts
@@ -0,0 +1,14 @@
+export const DAY_OF_WEEK = [
+  { id: 1, item: 'Monday' },
+  { id: 2, item: 'Tuesday' },
+  { id: 3, item: 'Wednesday' },
+  { id: 4, item: 'Thursday' },
+  { id: 5, item: 'Friday' },
+  { id: 6, item: 'Saturday' },
+  { id: 7, item: 'Sunday' },
+];
+
+export const AM_PM = [
+  { id: 1, item: 'AM', status: true },
+  { id: 2, item: 'PM', status: false },
+];
