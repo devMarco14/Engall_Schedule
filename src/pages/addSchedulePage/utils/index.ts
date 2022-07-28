ae9b224893b837800d5b25e918effd917d897a0f
HyeonJu <hhjc017@gmail.com>
2022-07-28 21:38:06 +0900

    #5 feature : 수업시간 선택 관련 유틸 함수 추가

---
+++ src/pages/addSchedulePage/utils/index.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+export const getIntervalArray = (
+    length: number,
+    interval: number,
+    isStartedWithZero: boolean,
+) => {
+    if (isStartedWithZero) {
+        return Array.from({ length }, (_value, index) => index * interval);
+    }
+    return Array.from({ length }, (_value, index) => (index + 1) * interval);
+};
+
+const CLASS_DURATION = 40;
+export const INITIAL_TIME = {
+    startTime: { hour: 0, minute: 0 },
+    endTime: { hour: 0, minute: 0 },
+};
+
+export const getEndTime = (startTime: { hour: number; minute: number }) => {
+    const endMinute = startTime.minute + CLASS_DURATION;
+    if (endMinute >= 60) {
+        return { hour: startTime.hour + 1, minute: endMinute % 60 };
+    }
+    return { hour: startTime.hour, minute: endMinute };
+};
