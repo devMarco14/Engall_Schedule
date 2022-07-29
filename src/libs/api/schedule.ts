8bb3c24dbee70bd41a3e3ab906ac4bfb12ffb70e
이지호 <cctv1998@naver.com>
2022-07-29 10:03:18 +0900

    feat : schedule API

---
+++ src/libs/api/schedule.ts
@@ -0,0 +1,19 @@
+import { HttpRequest } from 'hooks/httpRequest';
+import { Schedule } from 'types/schedule';
+
+const request = new HttpRequest();
+
+export const postSchedulesAPI = async (weekday: string, body: Schedule) => {
+  const response = await request.post(`/${weekday}`, body);
+  return response.data;
+};
+
+export const getSchedulesAPI = async (weekday: string) => {
+  const response = await request.get(`/${weekday}`);
+  return response.data;
+};
+
+export const deleteScheduleAPI = async (weekday: string, id: string) => {
+  const response = await request.delete(`/${weekday}/${id}`);
+  return response.data;
+};
